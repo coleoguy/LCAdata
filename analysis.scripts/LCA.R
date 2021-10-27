be6c035c9044e1a11bcf9712bc09a0845f431f41
Jorja Elliott <elliottjorja@gmail.com>
2021-10-27 09:57:23 -0500

    Create LCA.R

---
+++ analysis.scripts/LCA.R
@@ -0,0 +1,75 @@
+library(SAGA2)
+########################## Untransformed Data #######################
+beetle.dat <- read.csv("../data/saga.single.val.data.csv")
+  res <- LCA(data=beetle.dat,
+                   SCS="XY",parental = "calc",env=FALSE,
+                   max.pars = 7, ret.all=F)
+
+
+library(viridis)
+plot(res, col.ramp = viridis(100))
+
+barplot(res$best.models[[1]]$coefficients[3:4],
+        names=(c("Aa","AaAd")))
+#To visualize the model space only works well for low max.pars values
+#VisModelSpace(res)
+plot(res, min.vi = .55, main = "")
+
+skin.data <- read.csv("skin_data.csv")
+res <- LCA(data=skin.data, 
+             SCS="XY", parental = "calc", env=FALSE,
+             max.pars = 7, ret.all=F)
+library(viridis)
+plot(res, col.ramp = viridis(100))
+barplot(res$best.models[[1]]$coefficients[3:4],
+        names=(c("Aa", "AaAd")))
+
+library(SAGA2)
+
+#####
+
+#sticklebacks with rF1
+stickleback.dat <- read.csv("stickleback_data.csv")
+res <- LCA(data = stickleback.dat,
+           SCS="XY", parental = "calc", env=TRUE, 
+           max.pars = 6, ret.all=F, drop.pars = "Ca")
+library(viridis)
+plot(res, col.ramp=viridis(100), min.vi = .1)
+res$best.models
+
+
+#sticklebacks without rF1 -- use this one for analysis 
+stickleback.dat2 <- read.csv("stickleback_data2.csv")
+res <- LCA(data = stickleback.dat2,
+           SCS="XY", parental = "calc", env=TRUE, 
+           max.pars = 6, ret.all=F, drop.pars = "Ca")
+library(viridis)
+plot(res, col.ramp=viridis(100))
+
+#####
+
+#skin reflectance 
+skin.data <- read.csv("skin_data.csv")
+res <- LCA(data = skin.data,
+           SCS="XY", parental = "calc", env=TRUE, 
+           max.pars = 6, ret.all=F)
+plot(res, col.ramp=viridis(100))
+
+
+#####
+
+#seedweight of lima beans
+seedweight.data <- read.csv("seedweight.data.csv")
+res <- LCA(data= seedweight.data,
+               SCS="XY", parental = "calc", env=TRUE,
+               max.pars = 5, ret.all=F)
+plot(res, col.ramp=viridis(100))
+
+#####
+
+#time from seed to silking in zea mays
+silking.data <- read.csv("silking_data.csv")
+res <- LCA(data=silking.data,
+           SCS="XY", parental="calc", env=TRUE, 
+           max.pars = 5, ret.all=F)
+plot(res, col.ramp=viridis(100))
