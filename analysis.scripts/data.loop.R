803d09a1fb889070e00b295e9f58146ad8243929
Heath Blackmon <coleoguy@gmail.com>
2022-08-23 14:23:11 -0500

    Update data.loop.R

--- analysis.scripts/data.loop.R
+++ analysis.scripts/data.loop.R
@@ -1,18 +1,35 @@
 #storing results from each LCA run into csv to allow better access to data
 ####################
 
-
+library(SAGA2)
 chloro <- read.csv("../data/chloroLCA.csv")
 res <- LCA(data=chloro, 
            SCS="NSC", parental="calc", env=FALSE,
            max.pars=7, ret.all=F)
-foo <- data.frame(res$estimates[,-1])
-foo[3,] <- res$varimp[,2]
-row.names(foo)[3] <- "vi"
+full.table <- data.frame(res$estimates[,-1])
+full.table[3,] <- res$varimp[,2]
+row.names(full.table)[3] <- "vi"
 
 
-biomass <- read.csv("../data/biomassLCA.csv")
-res2 <- LCA(data=biomass,
-            SCS="NSC", parental="calc", env=FALSE,
-            max.pars = 7, ret.all = F)
+for(i in 1:length(file.names)){ # loop through all your datasets
+  biomass <- read.csv("../data/biomassLCA.csv")
+  res <- LCA(data=biomass,
+              SCS="NSC", parental="calc", env=FALSE,
+              max.pars = 7, ret.all = F)
+  start.row <- nrow(full.table) + 1
+  for(j in 2:ncol(res$estimates)){ # looping through the genetic effects
+    cur.col <- which(colnames(full.table) == colnames(res$estimates)[j])
+    ## TODO test the length of cur.col if it is zero then designate
+    ## the value of cur.col to be the next empty column
+    ## create the empty column and then give a column name to the new column too
+    ## extra credit if you see the way that you can do that all in one step
+    
+    full.table[start.row:(start.row+1), cur.col] <- res$estimates[,j]
+    full.table[(start.row + 2), cur.col] <- res$varimp[(j-1),2]
+  }
+}
+
+
+
+
 
