9a2faafcc50f2050cde2a783a8a7e187782d41f3
Jorja Elliott <elliottjorja@gmail.com>
2022-10-13 14:19:37 -0500

    update to data.loop

--- analysis.scripts/data.loop.R
+++ analysis.scripts/data.loop.R
@@ -7,8 +7,11 @@ data.files <- data.files[data.files != "ref.xlsx"] #remove the file with list of
 data.files <- data.files[data.files != "results.table.csv"]
 
 # this holds the sex chromosome system for each dataset
-# beetles silene and drosophila stickelbacks all XY run toads as NSC run any birds ZW
-SCS <- c(rep("NSC", 3),....)
+# beetles, silene,drosophila, and stickelbacks all XY 
+# run toads and plants as NSC
+SCS <- c(rep("NSC", 3), rep("XY", 21), rep("NSC",2), rep("XY",1), rep("NSC", 10), rep("XY", 1), 
+         rep("NSC", 10), rep("XY", 1), rep("NSC", 3), rep("XY", 6), rep("NSC", 10), rep("XY",1),
+         rep("NSC", 10), rep("XY",1), rep("NSC", 2), rep("XY", 2), rep("NSC", 5), rep("XY",2))
 
 d1 <- read.csv(paste("../data/", data.files[1], sep = "")) # lines 9-15 establish the basic data frame
 res <- LCA(data=d1, 
@@ -20,9 +23,18 @@ row.names(full.table)[3] <- "vi"
 
 for(i in 2:length(data.files)){ # loop through all datasets in LCA data folder
   cur.dat <- read.csv(paste("../data/", data.files[i], sep = ""))
+  # check to make sure F for female is not being converted to FALSE
+  
+  if(cur.dat$sex[1] == FALSE){ #fixing datasets where sex is all female and "F" for female is incorrectly called as "FALSE" 
+    cur.dat$sex <- rep("F", nrow(cur.dat))
+  }
+  max.pars <- nrow(cur.dat) - 1 #changing max pars for each dataset. max 7 pars for the datasets with many rows
+  if(max.pars > 7){
+    max.pars <- 7
+  }
   res <- LCA(data=cur.dat,
               SCS=SCS[i], parental="calc", env=FALSE,
-              max.pars = 7, ret.all = F)
+              max.pars = max.pars, ret.all = F)
   next.row <- nrow(full.table) + 1 # where in the table the loop should start putting data 
   for(j in 2:ncol(res$estimates)){ # looping through the genetic effects
     cur.col <- which(colnames(full.table) == colnames(res$estimates)[j]) #which col in full.table matches the col in res$estimates that has the data you want to place
@@ -41,7 +53,8 @@ for(i in 2:length(data.files)){ # loop through all datasets in LCA data folder
 full.table$dataset <- rep(data.files, each = 3) #add col 'dataset' with names of datasets corresponding to the data
 full.table$par <- rep(row.names(full.table)[1:3], times = nrow(full.table)/3) #repeat the first 3 row names (mwa, se, vi) for each dataset (every three rows)
 x <- ncol(full.table)
-full.table <- full.table[, c((x-1):x, 1:(x-2))] #recorganize the order of the cols
+full.table <- full.table[, c((x-1):x, 1:(x-2))] #reorganize the order of the cols
 row.names(full.table) <- NULL #remove row names that did not match; changed to just row numbers
 write.csv(full.table, "results.table.csv")
+
 
