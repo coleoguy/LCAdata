010adafa40b98c13e13bfa421e60d2f63c50fc24
Jorja Elliott <elliottjorja@gmail.com>
2022-12-12 15:07:55 -0600

    Update results.figures.R

--- results/results.figures.R
+++ results/results.figures.R
@@ -5,7 +5,7 @@ cat <- cat[complete.cases(dat), ]
 dat <- dat[complete.cases(dat), ]
 
 
-#plot of plant vs animals
+###plot of plant vs animals
 plot(0,0,col="white",xlim=c(-1,1),ylim=c(0,160),
      yaxt="n", xaxt="n", xlab="",ylab="")
 axis(side=1, at=c(-1,-.5,.5,1), c("100%","50%","50%","100%"))
@@ -20,9 +20,7 @@ legend("bottomleft", legend=c("animal", "plant"),
        fill=c("red", "blue"), cex=0.8)
 
 
-
-
-#plot of LH vs M traits
+###plot of LH vs M traits
 plot(0,0,col="white",xlim=c(-1,1),ylim=c(0,160),
      yaxt="n", xaxt="n", xlab="",ylab="")
 axis(side=1, at=c(-1,-.5,.5,1), c("100%","50%","50%","100%"))
@@ -37,9 +35,7 @@ legend("bottomleft", legend=c("LH", "M"),
        fill=c("red", "blue"), cex=0.8)
 
 
-
-
-#plot of within or between species
+###plot of within or between species
 plot(0,0,col="white",xlim=c(-1,1),ylim=c(0,160),
      yaxt="n", xaxt="n", xlab="",ylab="")
 axis(side=1, at=c(-1,-.5,.5,1), c("100%","50%","50%","100%"))
