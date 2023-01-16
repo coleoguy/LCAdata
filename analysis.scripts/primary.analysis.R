dd2b6c34dc07573af014e85a4af6b311ed0032bf
Heath Blackmon <coleoguy@gmail.com>
2023-01-16 09:48:35 -0600

    so much more data

--- analysis.scripts/primary.analysis.R
+++ analysis.scripts/primary.analysis.R
@@ -7,64 +7,42 @@
 # of the ref file that contains dataset specific info for SCS, cmatrix, etc.
 library(SAGA2)
 ref <- read.csv("../all.data/ref.csv")
+ref <- ref[order(ref$new.file.name),]
 res <- list()
 maximum.allowed <- 7
-######### standard files ######### 
-#data in standard folder
-data.files <- list.files("../all.data/standard")
+data.files <- list.files("../all.data/data/")
+## which(data.files != ref$new.file.name)
 for(i in 1:length(data.files)){
-  cat(paste("analyzing standard dataset", i, "of", length(data.files), "\r"))
+  cat(paste("analyzing dataset", i, "of", length(data.files), "\r"))
   flush.console()
-  cur.dat <- read.csv(paste("../all.data/standard/", data.files[i],sep = ""))
+  cur.dat <- read.csv(paste("../all.data/data/", data.files[i],sep = ""))
   # assuring that F for female is not stored as False 
-  if(cur.dat$sex[1] == FALSE){ 
-    cur.dat$sex <- rep("F", nrow(cur.dat))
+  if(ncol(cur.dat) > 3){
+    if(cur.dat$sex[1] == FALSE){ 
+      cur.dat$sex <- rep("F", nrow(cur.dat))
+    }
   }
   # max pars max = 7 otherwise 2 minus cohorts
   max.pars <- nrow(cur.dat) - 2 
   if(max.pars > 7) max.pars <- maximum.allowed
-  res[[i]] <- LCA(data=cur.dat,
-              SCS=ref[,5][i], parental="calc", env=FALSE,
-              max.pars = max.pars, ret.all = F, messages=F)[c(4,6)]
-}
-######### standard files done ######### 
-############# cmat files ##############
-data.files <- list.files("../all.data/cmat/data/")
-# loop through all datasets in PSU data folder
-for(i in 1:length(data.files)){ 
-  cat(paste("analyzing cmat dataset", i, "of", length(data.files), "\r"))
-  flush.console()
-  cur.dat <- read.csv(paste("../all.data/cmat/data/", data.files[i], sep = ""))
-  cname <- ref$cmat..if.assigned.[which(ref$new.file.name == data.files[i])]
-  cmat <- as.matrix(read.csv(paste("../all.data/cmat/cmats/", cname, sep = "")))
-  # max pars max = 7 otherwise 2 minus cohorts
-  max.pars <- nrow(cur.dat) - 2 
-  if(max.pars > 7) max.pars <- maximum.allowed
-  res[[length(res) + 1]] <- LCA(cur.dat, Cmatrix = cmat, messages=F)[c(4,6)]
-}
-########### cmat files done ###########
-############### PSU files #############
-data.files <- list.files("../all.data/PSU")
-# loop through all datasets in PSU data folder
-for(i in 1:length(data.files)){ 
-  cat(paste("analyzing PSU dataset", i, "of", length(data.files), "\r"))
-  flush.console()
-  cur.dat <- read.csv(paste("../all.data/PSU/", data.files[i], sep = ""))
-  # assuring that F for female is not stored as False 
-  if (cur.dat$sex[1] == FALSE) {
-    cur.dat$sex <- rep("F", nrow(cur.dat))
+  if(ref$data.type..standard..PSU..cmat[i] == "standard"){
+    res[[i]] <- LCA(data=cur.dat,
+                    SCS=ref[,5][i], parental="calc", env=FALSE,
+                    max.pars = max.pars, ret.all = F, messages=F)[c(4,6)]
+  }  
+  if(ref$data.type..standard..PSU..cmat[i] == "cmat"){
+    cname <- ref$cmat..if.assigned.[which(ref$new.file.name == data.files[i])]
+    cmat <- as.matrix(read.csv(paste("../all.data/cmat/", cname, sep = "")))
+    res[[i]] <- LCA(cur.dat, Cmatrix = cmat, messages=F)[c(4,6)]
   }
-  max.pars <- nrow(cur.dat) - 2 
-  keep.pars <- c("Aa","Ad","AaAa","AaAd","AdAd")
-  res[[length(res) + 1]] <- LCA(data = cur.dat,
-                                parental = "calc", 
-                                env = FALSE, 
-                                max.pars = max.pars, 
-                                ret.all = F,
-                                keep.pars = keep.pars,
-                                messages = F)[c(4, 6)]
+  if(ref$data.type..standard..PSU..cmat[i] == "PSU"){
+    keep.pars <- c("Aa","Ad","AaAa","AaAd","AdAd")
+    res[[i]] <- LCA(data = cur.dat, parental = "calc",env = FALSE, 
+                    max.pars = max.pars, ret.all = F, keep.pars = keep.pars,
+                    messages = F)[c(4, 6)]
+  }
 }
-############# PSU files done ##########
+
 save(res, file="../results/full.run.RData")
 
 
