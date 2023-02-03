6303dd922e468fd6344ce4a187bd0c9fe86f5388
Max Chin <maxchin@tamu.edu>
2023-02-03 14:46:03 -0600

    update PSU vs full plots

--- figures/psu.vs.full.R
+++ figures/psu.vs.full.R
@@ -5,12 +5,9 @@
 
 #PSU reduced dataset
 dat.psu <- read.csv("../results/reduced.results.csv")
-dat.psu <- dat.psu[! is.na(dat.psu$add),]
-
 
 #Non-PSU dataset
 dat <- read.csv("../results/complete.results.csv")
-dat <- dat[! is.na(dat$add),]
 
 #### SUBSET TO JUST STANDARD AND CMAT ####
 
@@ -20,14 +17,51 @@ dat.psu <- dat.psu[!dat.psu$method %in% "PSU",]
 #Non-PSU
 dat <- dat[!dat$method %in% "PSU",]
 
-#### GET NON-ADDITIVE SUMS AND X PROPORTIONS ####
+#Temporarily convert NAs to 0
+dat.psu[which(is.na(dat.psu$add)),c(1,2,3)] <- 0
+dat[which(is.na(dat$add)),c(1,2,3)] <- 0
+
+#### CORRELATION SCATTERPLOT ####
+
+cairo_pdf(paste0("../figures/PSU_Fullcor.pdf"),
+          width = 5,
+          height = 5)
+
+plot(x=jitter(dat.psu$epi,20),
+     y=jitter(dat$epi,20),
+     xlab = "PSU model proportion epistasis",
+     ylab = "Full model proportion epistasis",
+     pch=16,
+     cex=1,
+     col= rgb(red = 63/255,
+              green = 71/255,
+              blue = 136/255,
+              alpha=0.5))
+
+dev.off()
+
+#### GET EPISTASIS SUMS AND X PROPORTIONS ####
+
+#Remove data with no significant models
+dat.psu <- dat.psu[-which(dat.psu$add == 0 &
+                          dat.psu$dom == 0 &
+                          dat.psu$epi == 0),]
+
+dat <- dat[-which(dat$add == 0 &
+                  dat$dom == 0 &
+                  dat$epi == 0),]
+
+#Get sums and x proportions
 psu.sums <- sort(dat.psu[,3])
 full.sums <- sort(dat[,3])
 psux <- seq(from=0, to=100, length.out=length(psu.sums))
 fullx <- seq(from=0, to=100, length.out=length(full.sums))
 
 #### PLOT AND SAVE####
-cairo_pdf(paste0("../figures/PSU_Fullplot.pdf"))
+
+cairo_pdf(paste0("../figures/PSU_Fullplot.pdf"),
+          width = 5,
+          height = 5)
 
 plot(0,0,col="white",xlim=c(0,100),ylim=c(0,1),
      xaxt="n", xlab="proportion of datasets analyzed",
